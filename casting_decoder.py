98d626f49649f4e4ece2452400c51b07c8d5d22f
clutch70 <b.custard@outlook.com>
2023-12-06 09:01:58 -0600

    Refactor to use single search field

--- casting_decoder.py
+++ casting_decoder.py
@@ -1,91 +1,48 @@
+import sys
 import pandas as pd
-import argparse
 
-def load_data(file_path):
-    """ Load the CSV data into a DataFrame. """
-    try:
-        return pd.read_csv(file_path)
-    except Exception as e:
-        print(f"Error loading file: {e}")
-        return None
 
-def is_match(column_value, input_value):
-    """ Check if the input_value matches any of the comma-separated values in column_value or if the column_value is blank. """
-    # Treat blank or NA column_value as a match (regardless of input_value)
-    if pd.isna(column_value) or column_value == '':
-        return True
+# Function to get search terms
+def get_search_terms():
+    if len(sys.argv) > 1:
+        # Use the command line argument if provided
+        search_string = sys.argv[1]
+    else:
+        # Prompt the user for input if no command line argument is provided
+        search_string = input("Enter search terms separated by spaces: ")
 
-    # Convert column_value to string and check for a partial match
-    column_value = str(column_value)
-    return any(input_value in item for item in column_value.split(','))
+    # Split the search string into terms
+    return search_string.split()
 
-def highlight_matched_input(input_value, column_value):
-    """ Highlight the matched part in the column value and return if a match was found. """
-    if not input_value or pd.isna(column_value) or column_value == '':
-        return column_value, False
-    if input_value in column_value:
-        return column_value.replace(input_value, f'*{input_value}*'), True
-    return column_value, False
-
-def get_data_or_default(value):
-    """ Return the value or 'NO DATA' if the value is blank or NaN. """
-    return value if not pd.isna(value) and value != '' else 'NO DATA'
-
-def find_engine(data, block, right_head, left_head, crank, hollander):
-    """ Search for the engine based on casting numbers allowing for partial matches. """
-    matches = data.apply(lambda row: is_match(row['BlockCasting'], block) and
-                                     is_match(row['RightHeadCasting'], right_head) and
-                                     is_match(row['LeftHeadCasting'], left_head) and
-                                     is_match(row['CrankCasting'], crank) and
-                                     is_match(row['Hollander'], hollander), axis=1)
-    return data[matches]
-
-def get_user_input(input_field):
-    return input(f"Enter {input_field} (part or full, leave blank if unknown): ")
 
 def main():
-    parser = argparse.ArgumentParser(description='Engine Identifier')
-    parser.add_argument('--block', help='Block Casting number (part or full)', default='')
-    parser.add_argument('--right_head', help='Right Head Casting number (part or full)', default='')
-    parser.add_argument('--left_head', help='Left Head Casting number (part or full)', default='')
-    parser.add_argument('--crank', help='Crank Casting number (part or full)', default='')
-    parser.add_argument('--hollander', help='Hollander Interchange number (part or full)', default='')
-    args = parser.parse_args()
+    # Load the CSV file
+    # Ensure to replace 'path_to_csv.csv' with the actual path to your CSV file
+    df = pd.read_csv('qbinv_cores.csv')
 
-    if not any(vars(args).values()):
-        args.block = get_user_input("Block Casting number")
-        args.right_head = get_user_input("Right Head Casting number")
-        args.left_head = get_user_input("Left Head Casting number")
-        args.crank = get_user_input("Crank Casting number")
-        args.hollander = get_user_input("Hollander Interchange number, suffix only")
-    data_file = 'bom.csv'  # Replace with your CSV file path
-    data = load_data(data_file)
+    # Get search terms from command line arguments or user input
+    search_terms = get_search_terms()
 
-    if data is not None:
-        matching_engines = find_engine(data, args.block, args.right_head, args.left_head, args.crank, args.hollander)
-        if not matching_engines.empty:
-            for index, row in matching_engines.iterrows():
-                block_cast, block_match = highlight_matched_input(args.block, row['BlockCasting'])
-                right_head_cast, right_head_match = highlight_matched_input(args.right_head, row['RightHeadCasting'])
-                left_head_cast, left_head_match = highlight_matched_input(args.left_head, row['LeftHeadCasting'])
-                crank_cast, crank_match = highlight_matched_input(args.crank, row['CrankCasting'])
-                hollander_num, hollander_match = highlight_matched_input(args.hollander, row['Hollander'])
+    # Start with a mask that selects everything
+    mask = pd.Series([True] * len(df))
 
-                if any([block_match, right_head_match, left_head_match, crank_match, hollander_match]):
-                    print(f"Manufacturer: {get_data_or_default(row['Make'])}")
-                    print(f"Years: {get_data_or_default(row['Years'])}")
-                    print(f"Engine Type: {get_data_or_default(row['Engine'])}")
-                    print(f"Block Casting: {get_data_or_default(block_cast)}")
-                    print(f"Right Head Casting: {get_data_or_default(right_head_cast)}")
-                    print(f"Left Head Casting: {get_data_or_default(left_head_cast)}")
-                    print(f"Crank Casting: {get_data_or_default(crank_cast)}")
-                    print(f"Hollander: {get_data_or_default(hollander_num)}")
-                    print(f"Notes: {get_data_or_default(row['Notes'])}")
-                    print("-----------")
-        else:
-            print("No matching engine found.")
-    else:
-        print("Failed to load data.")
+    # Update the mask to select only rows that contain all search terms in the Description
+    for term in search_terms:
+        mask &= df['Description'].str.contains(term, case=False, na=False)
 
-if __name__ == "__main__":
+    # Apply the mask to the dataframe to filter the results
+    filtered_df = df[mask]
+
+    # Convert the filtered dataframe to a dictionary
+    parts_dict = filtered_df.set_index('PartNumber').T.to_dict('dict')
+
+    # Display the dictionary
+    for part_number, details in parts_dict.items():
+        print(f"Part Number: {part_number}")
+        for key, value in details.items():
+            print(f"  {key}: {value}")
+        print()  # Add a new line for readability between parts
+
+
+if __name__ == '__main__':
     main()
--- index.php
+++ index.php
@@ -2,59 +2,33 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <title>Casting Decoder</title>
+    <title>CCS - Engine Decoder</title>
+    <link rel="stylesheet" href="style.css"> <!-- Link to external CSS file -->
 </head>
 <body>
     <header>
-        <h1>Custard Core</h1>
+        <h1>Custard Core Engine Decoder</h1>
     </header>
-    <main>
-        <section class="form-container">
-            <h2>Casting Decoder</h2>
-            <p>Enter the casting numbers below to decode the casting. Partial numbers are ok, but are case sensitive.</p>
-            <p>Introduce los números de fundición a continuación para descifrar el molde. Los números parciales están bien, pero son sensibles a mayúsculas y minúsculas.</p>
-            <form method="post">
-                <!-- Add form fields here -->
-                <input type="text" name="block" placeholder="Block">
-                <input type="text" name="right_head" placeholder="Right Head">
-                <input type="text" name="left_head" placeholder="Left Head">
-                <input type="text" name="crank" placeholder="Crank">
-                <input type="text" name="hollander" placeholder="Hollander">
-                <button type="submit">Submit</button>
-            </form>
-            <?php
-                // Check if form is submitted
-                if ($_SERVER["REQUEST_METHOD"] == "POST") {
-                    // Collect input from the form and build the command
-                    $args = [];
-                    if (!empty($_POST['block'])) {
-                        $args[] = '--block ' . escapeshellarg($_POST['block']);
-                    }
-                    if (!empty($_POST['right_head'])) {
-                        $args[] = '--right_head ' . escapeshellarg($_POST['right_head']);
-                    }
-                    if (!empty($_POST['left_head'])) {
-                        $args[] = '--left_head ' . escapeshellarg($_POST['left_head']);
-                    }
-                    if (!empty($_POST['crank'])) {
-                        $args[] = '--crank ' . escapeshellarg($_POST['crank']);
-                    }
-                    if (!empty($_POST['hollander'])) {
-                        $args[] = '--hollander ' . escapeshellarg($_POST['hollander']);
-                    }
-
-                    $command = 'python3 casting_decoder.py ' . implode(' ', $args);
-                    $output = shell_exec($command);
-
-                    // Display the output
-                    echo "<div class='output'><pre>$output</pre></div>";
-
-                }
-            ?>
-        </section>
-    </main>
+    <div class="form-container">
+        <div class="instructions">
+            <h2>Instructions:</h2>
+            <p>Enter search terms to find in the CSV description field.</p>
+        </div>
+        <form action="" method="post">
+            <input type="text" name="search_terms" placeholder="Enter terms...">
+            <button type="submit" name="submit">Search</button>
+        </form>
+    </div>
+    <div class="output">
+        <?php
+        if (isset($_POST['submit'])) {
+            $search_terms = escapeshellarg($_POST['search_terms']);
+            // Call the Python script with the search terms
+            $command = escapeshellcmd("python engine_lookup.py $search_terms");
+            $output = shell_exec($command);
+            echo htmlspecialchars($output);
+        }
+        ?>
+    </div>
 </body>
 </html>
-
