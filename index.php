79a25d0f7ef1fade53ac6f773f3ab331f67249e4
clutch70 <b.custard@outlook.com>
2023-12-06 10:05:48 -0600

    Implement 'shopping cart'

--- index.php
+++ index.php
@@ -1,3 +1,46 @@
+<?php
+session_start(); // Start the session.
+
+// Function to add to cart.
+function addToCart($partNumber, $description, $cost, $price) {
+    $_SESSION['cart'][$partNumber] = [
+        'description' => $description,
+        'cost' => $cost,
+        'price' => $price
+    ];
+}
+
+// Function to export cart.
+function exportCart() {
+    header('Content-Type: text/csv');
+    header('Content-Disposition: attachment; filename="cart.csv"');
+
+    $output = fopen('php://output', 'w');
+    fputcsv($output, ['Part Number', 'Description', 'Cost', 'Price']); // Column headings
+
+    // Output the cart items.
+    foreach ($_SESSION['cart'] as $partNumber => $item) {
+        fputcsv($output, [$partNumber, $item['description'], $item['cost'], $item['price']]);
+    }
+    fclose($output);
+    exit();
+}
+
+// Check for export action
+if(isset($_GET['action']) && $_GET['action'] == 'export') {
+    exportCart();
+}
+
+// Check for add to cart action
+if(isset($_POST['add_to_cart'])) {
+    $partNumber = $_POST['part_number'];
+    $description = $_POST['description']; // You'll need to get these details from your form or from the CSV.
+    $cost = $_POST['cost'];
+    $price = $_POST['price'];
+    addToCart($partNumber, $description, $cost, $price);
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -30,5 +73,36 @@
         }
         ?>
     </div>
+    <!-- Cart Display -->
+<div class="cart-container">
+    <h2>Current Load</h2>
+    <table>
+        <thead>
+            <tr>
+                <th>Part Number</th>
+                <th>Description</th>
+                <th>Cost</th>
+                <th>Price</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php if (!empty($_SESSION['cart'])): ?>
+                <?php foreach ($_SESSION['cart'] as $partNumber => $item): ?>
+                    <tr>
+                        <td><?php echo htmlspecialchars($partNumber); ?></td>
+                        <td><?php echo htmlspecialchars($item['description']); ?></td>
+                        <td><?php echo htmlspecialchars($item['cost']); ?></td>
+                        <td><?php echo htmlspecialchars($item['price']); ?></td>
+                    </tr>
+                <?php endforeach; ?>
+            <?php else: ?>
+                <tr>
+                    <td colspan="4">Your cart is empty.</td>
+                </tr>
+            <?php endif; ?>
+        </tbody>
+    </table>
+    <a href="?action=export">Export as CSV</a>
+</div>
 </body>
 </html>
