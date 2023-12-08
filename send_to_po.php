5b022b7558da45b74d0a9e111f636a1377020b8e
clutch70 <67942178+clutch70@users.noreply.github.com>
2023-12-08 07:46:01 -0600

    dev sendToPoButton

--- send_to_po.php
+++ send_to_po.php
@@ -2,8 +2,12 @@
 $cart_data = $_POST['cart_data'];
 $po_number = $_POST['po_number'];
 
+// Escape the arguments
+$cart_data_escaped = escapeshellarg($cart_data);
+$po_number_escaped = escapeshellarg($po_number);
+
 // Call the Python script with the cart data and the PO Number as arguments
-$command = escapeshellcmd("python3 add_po_item.py $po_number $cart_data");
+$command = "python3 add_po_item.py $po_number_escaped $cart_data_escaped";
 echo $command;
 $output = shell_exec($command);
 echo $output;
