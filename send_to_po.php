a98fc3452cebddf21e9646e643eb2a90b2bd74af
clutch70 <67942178+clutch70@users.noreply.github.com>
2023-12-08 07:44:21 -0600

    dev sendToPoButton

--- send_to_po.php
+++ send_to_po.php
@@ -4,7 +4,9 @@ $po_number = $_POST['po_number'];
 
 // Call the Python script with the cart data and the PO Number as arguments
 $command = escapeshellcmd("python3 add_po_item.py $po_number $cart_data");
+echo $command;
 $output = shell_exec($command);
+echo $output;
 
 // Handle the output...
 ?>
